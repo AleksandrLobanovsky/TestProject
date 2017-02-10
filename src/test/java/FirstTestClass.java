b262443f0175e0e081a964b753417ad4674d6b64
Aleksandr <alobanovskiy@biarum.com>
2017-02-10 19:23:59 +0300

    config for tests

--- src/test/java/FirstTestClass.java
+++ src/test/java/FirstTestClass.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,17 +17,20 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class FirstTestClass {
-    public WebDriver driver;
-    public String userID;
-    public String basicUrl ="https://dev3.pdffiller.com/";
-    public String currentDriver;
+    private WebDriver driver;
+    private String userID;
+    private String basicUrl;
+    private String currentDriver;
+    private String userName;
+    private String userPassword;
+    private  String driverPath;
     @Test
     public void main() {
 
         LoginClass loginObject = new LoginClass(driver);
         loginObject.moveToLoginPage();
-        loginObject.feelUserNameField("lobanovsky.a.o@gmail.com");
-        loginObject.feelPasswordField("Test1234");
+        loginObject.feelUserNameField(userName);
+        loginObject.feelPasswordField(userPassword);
         loginObject.loginSubmit();
         MyFormsClass myFormsObject = new MyFormsClass(driver,basicUrl+"en/forms.htm");
         userID = myFormsObject.GetUserId();
@@ -35,10 +40,7 @@ public class FirstTestClass {
     @BeforeMethod
     public void beforeMethod() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\alobanovskiy\\Documents\\Visual Studio 2015\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get(basicUrl);
+
         try {
             File fXmlFile = new File("src\\test\\java\\TestConfig.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -51,15 +53,41 @@ public class FirstTestClass {
             for (int temp = 0; temp < nList.getLength(); temp++) {
 
                 Node nNode = nList.item(temp);
-
                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
                     Element eElement = (Element) nNode;
-
                     currentDriver = eElement.getElementsByTagName("currentWebDriver").item(0).getTextContent();
-                    //System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+                    System.out.println(currentDriver);
+                    System.out.println(currentDriver.toCharArray()[0]);
+                    switch (currentDriver.toCharArray()[0]){
+                        case 'C':
+                            driverPath = eElement.getElementsByTagName("chromePath").item(0).getTextContent();
+                            System.setProperty("webdriver.chrome.driver", driverPath);
+                            driver = new ChromeDriver();
+                            break;
+                        case 'F':
+                            driverPath = eElement.getElementsByTagName("firefoxPath").item(0).getTextContent();
+                            System.setProperty("firefoxdriver.chrome.driver", driverPath);
+                            driver = new FirefoxDriver();
+                            break;
+                        case 'E':
+                            driverPath = eElement.getElementsByTagName("explorerPath").item(0).getTextContent();
+                            System.setProperty("internetexplorer.chrome.driver", driverPath);
+                            driver = new InternetExplorerDriver();
+                            break;
+                        default:
+                            System.setProperty("webdriver.chrome.driver", driverPath);
+                            driverPath = eElement.getElementsByTagName("chromePath").item(0).getTextContent();
+                            driver = new ChromeDriver();
+                            break;
+
+                    }
+
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    basicUrl = eElement.getElementsByTagName("basicURL").item(0).getTextContent();
+                    userName = eElement.getElementsByTagName("userName").item(0).getTextContent();
+                    userPassword = eElement.getElementsByTagName("password").item(0).getTextContent();
 
                 }
             }
@@ -71,6 +99,8 @@ public class FirstTestClass {
             System.out.println("Error while reading config");
         }
 
+
+        driver.get(basicUrl);
     }
 
     @AfterMethod
